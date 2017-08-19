42f97afc4d8d56777af89129ec06f7c7c201b847
Elvin <elvin.granat@gmail.com>
2017-08-19 18:56:38 +0200

    Initial structure complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FirebaseService } from './services/firebase.service';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdListComponent } from './components/ad-list/ad-list.component';
+import { AdCreateComponent } from './components/ad-create/ad-create.component';
+import { AdEditComponent } from './components/ad-edit/ad-edit.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AdComponent } from './components/ad/ad.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'annonser', component: AdListComponent},
+  {path: 'skapa-annons', component: AdCreateComponent},
+  {path: 'annons/:id', component: AdComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdListComponent,
+    AdCreateComponent,
+    AdEditComponent,
+    ProfileComponent,
+    SettingsComponent,
+    AdComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [],
+  providers: [
+    FirebaseService,
+    AngularFireAuth,
+    AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA4zpFd8u3tp5GptJJ2uNuZREVa4Vn9KDs',
+    authDomain: 'plantify-64319.firebaseapp.com',
+    databaseURL: 'https://plantify-64319.firebaseio.com',
+    projectId: 'plantify-64319',
+    storageBucket: '',
+    messagingSenderId: '1080377796316'
+  }
 };
